020bbdd4b8a6c54b4f25465ca0b78a0d9df6960a
ali <aliahmed408@outlook.com>
2025-01-01 21:17:34 +1100

    using fontawesome to show chess pieces now

--- frontend/src/Chess/Board.jsx
+++ frontend/src/Chess/Board.jsx
@@ -1,23 +1,30 @@
 import React from "react";
-import Pawn from "../Images/Pawn.svg";
-import Rook from "../Images/Rook.svg";
-import Knight from "../Images/Knight.svg";
-import Bishop from "../Images/Bishop.svg";
-import Queen from "../Images/Queen.svg";
-import King from "../Images/King.svg";
+import {
+    KingIcon,
+    QueenIcon,
+    BishopIcon,
+    KnightIcon,
+    RookIcon,
+    PawnIcon,
+} from "./Icons.jsx";
 import boardInit from "./InitializeBoard.tsx";
 
-const pieceImages = {
-    Pawn: Pawn,
-    Rook: Rook,
-    Knight: Knight,
-    Bishop: Bishop,
-    Queen: Queen,
-    King: King,
+const pieceComponents = {
+    Pawn: PawnIcon,
+    Rook: RookIcon,
+    Knight: KnightIcon,
+    Bishop: BishopIcon,
+    Queen: QueenIcon,
+    King: KingIcon,
 };
 
 const Board = () => {
     const chessBoard = boardInit();
+
+    const renderPiece = (piece) => {
+        const PieceIcon = pieceComponents[piece.type];
+        return PieceIcon ? <PieceIcon color={piece.color} /> : null;
+    };
 
     return (
         <div className='grid grid-cols-8 grid-rows-8 w-1/2 h-3/4 border'>
@@ -30,13 +37,7 @@ const Board = () => {
                             cell.color === "white" ? "#cdb081" : "#483624",
                     }}
                 >
-                    {cell.currentPiece.type && (
-                        <img
-                            src={pieceImages[cell.currentPiece.type]}
-                            className='scale-60 mb-2'
-                            alt={cell.currentPiece.type}
-                        />
-                    )}
+                    {renderPiece(cell.currentPiece)}
                 </div>
             ))}
         </div>
